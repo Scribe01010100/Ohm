260cbae274b11e24c61fdce493bf0b16e3be3a89
Scribe.dev <144683280+Scribe01010100@users.noreply.github.com>
2024-08-05 11:44:24 +0530

    Update app.js

--- Web Panel/app.js
+++ Web Panel/app.js
@@ -2,13 +2,13 @@
 
 
     var firebaseConfig = {
-    apiKey: "AIzaSyDnV5Vc66YZd8EKfo2Et0weMwvKNiqzAbg",
-    authDomain: "ohm-f1e87.firebaseapp.com",
-    databaseURL: "https://ohm-f1e87-default-rtdb.firebaseio.com",
-    projectId: "ohm-f1e87",
-    storageBucket: "ohm-f1e87.appspot.com",
-    messagingSenderId: "438858521869",
-    appId: "1:438858521869:web:10622454d320259eee6444"
+    apiKey: "api_key",
+    authDomain: "authdomain",
+    databaseURL: "databaseurl",
+    projectId: "projectid",
+    storageBucket: "storagebucket",
+    messagingSenderId: "massageid",
+    appId: "appid"
   }; 
  
 ////Do Not Edit Below This
